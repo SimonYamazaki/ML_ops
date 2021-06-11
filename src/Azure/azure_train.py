e76b45bc428189be702eb2958fce408353578c09
Simon Yamazaki <simon-yamaza@hotmail.com>
2021-06-11 15:52:20 +0200

    trying another method for conda deps

--- src/Azure/azure_train.py
+++ src/Azure/azure_train.py
@@ -16,7 +16,13 @@ with open('req.txt', 'w') as f:
     f.writelines(req_lines)
 
 #azure_env = Environment.from_pip_requirements(name="azure-env", file_path="req.txt")
-azure_env = Environment.from_conda_specification(name='azure-env', file_path='../../ml_ops2.yml')
+#azure_env = Environment.from_conda_specification(name='azure-env', file_path='../../ml_ops.yml')
+
+azure_env = Environment("azure_env")
+
+# Ensure the required packages are installed (we need pip, scikit-learn and Azure ML defaults)
+packages = CondaDependencies.create(conda_dependencies_file_path='../../ml_ops.yml')
+azure_env.python.conda_dependencies = packages
 
 # Create a script config
 script_config = ScriptRunConfig(source_directory='',
