0ed200e411fa02a09eb50440fb61271980460b35
Simon Yamazaki <simon-yamaza@hotmail.com>
2021-06-15 14:49:42 +0200

    first draft of model deployment scripts

--- src/Azure/azure_infer.py
+++ src/Azure/azure_infer.py
@@ -1,5 +1,9 @@
+import os
 from azureml.core import Workspace, Model
 from azureml.core.authentication import InteractiveLoginAuthentication
+from azureml.core.webservice import AciWebservice
+from azureml.core.model import InferenceConfig
+
 ia = InteractiveLoginAuthentication(tenant_id='f251f123-c9ce-448e-9277-34bb285911d9')
 
 ws = Workspace.get(name='ML_ops',
@@ -12,4 +16,22 @@ dir_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__fi
 #model = Model.register(ws, model_name="working_MNIST_CNN", model_path=dir_path+"/models/checkpoint_15062021123318.pth")
 
 model = ws.models['working_MNIST_CNN']
+
+azure_env = Environment.from_pip_requirements(name="azure-env", file_path=dir_path+'/src/azure/req3.txt')
+
+# Configure the scoring environment
+inference_config = InferenceConfig(runtime= "python",
+                                   entry_script='azure_infer_file.py',
+                                   environment=azure_env)
+
+deployment_config = AciWebservice.deploy_configuration(cpu_cores = 1, memory_gb = 1)
+
+service_name = "MNIST-service"
+
+service = Model.deploy(ws, service_name, [model], inference_config, deployment_config)
+
+service.wait_for_deployment(True)
+
+
+
 
--- src/Azure/azure_infer_file.py
+++ src/Azure/azure_infer_file.py
@@ -2,24 +2,45 @@ import json
 import joblib
 import numpy as np
 from azureml.core.model import Model
+import torch 
+from src.models.model import MyAwesomeModel
+
+def load_checkpoint(filepath, get_feature_layer=False):
+    checkpoint = torch.load(filepath)
+    model = MyAwesomeModel(
+        checkpoint["image_dim"],
+        checkpoint["kernel_size"],
+        checkpoint["filters"],
+        checkpoint["fc_features"],
+        checkpoint["n_classes"],
+        get_feature_layer=get_feature_layer,
+    )
+    model.load_state_dict(checkpoint["state_dict"])
+    return model
 
 # Called when the service is loaded
 def init():
     global model
     # Get the path to the deployed model file and load it
-    model_path = Model.get_model_path('diabetes_model')
-    model = joblib.load(model_path)
+    model_path = Model.get_model_path('working_MNIST_CNN')
+    model = load_checkpoint(model_path)
+    #model = joblib.load(model_path)
 
 # Called when a request is received
 def run(raw_data):
     # Get the input data as a numpy array
     data = np.array(json.loads(raw_data)['data'])
+    dataT = torch.from_numpy(data)
     # Get a prediction from the model
-    predictions = model.predict(data)
+    with torch.no_grad():
+        model.eval()
+        ps = torch.exp(model(dataT))
+        top_p, top_class = ps.topk(1, dim=1)
+    
     # Get the corresponding classname for each prediction (0 or 1)
-    classnames = ['not-diabetic', 'diabetic']
+    classnames = ['0', '1','2','3','4','5','6','7','8','9']
     predicted_classes = []
-    for prediction in predictions:
+    for prediction in top_class:
         predicted_classes.append(classnames[prediction])
     # Return the predictions as JSON
     return json.dumps(predicted_classes)
